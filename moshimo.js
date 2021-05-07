2d70ee30506b62bd80946f4259197e7fa6f14e74
Gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-05-07 22:30:50 +0900

    Created scripts

--- moshimo.js
+++ moshimo.js
@@ -1,12 +1,12 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const path = require('path');
 
 const secret = require('./secret.json');
 const puppeteer_config = require("./puppeteer_config.json");
 
 const targetDomain = "https://af.moshimo.com";
-let browser;
-let page;
+const snippetsDir = `snippets-${new Date().toISOString()}`;
 
 const setUpBrowser = async () => {
   const browser = await puppeteer.launch(puppeteer_config);
@@ -16,13 +16,15 @@ const setUpBrowser = async () => {
   return browser;
 }
 
-const login = async () => {
+const login = async (page) => {
   const cookies = require('./cookies.json');
 
   if (Object.keys(cookies).length) {
-    // await page.setCookie(...cookies); // cookie は期限切れの可能性あり
-    return cookies;
+    console.log("Preexisting cookies found, skip log in");
+    return cookies; //cookie は期限切れの可能性あり
   }
+
+  console.log(`Logging into ${targetDomain}`);
 
   const loginPageURL = `${targetDomain}/af/shop/login`;
   const loginFormAccount = 'input.input-text[name="account"]';
@@ -40,16 +42,52 @@ const login = async () => {
   const currentCookies = await await page.cookies();
 
   fs.writeFileSync('./cookies.json', JSON.stringify(currentCookies));
-
+  console.log(`Logged into ${targetDomain}`);
   return currentCookies;
 }
 
-const scrapeAnchorTags = (cookies) => {
+const generateHTML = async (searchWord, htmlSnippets) => {
+  let html = "";
+  htmlSnippets.forEach((snippet) => {
+    html += `
+      <div class="text-center me-2 mb-2">
+        ${snippet}
+      </div>
+    `;
+  });
+  await fs.promises.writeFile(`${snippetsDir}/_${searchWord}.html`, html);
+  console.log(`Generated: ${snippetsDir}/_${searchWord}.html`);
+}
+
+const scrapeBySearchWord = (browser, page, searchWord, cookies, limit = 5) => {
+  const htmlSnippets = [];
   return new Promise(async (resolve, reject) => {
     try {
       if (Object.keys(cookies).length) {
         await page.setCookie(...cookies); // cookie は期限切れの可能性あり
+      } else {
+        return reject("Cookies MUST be provided to search the website.");
       }
+
+      await page.goto(`${targetDomain}/af/shop/promotion/source/rakuten?promotion_id=54&shop_site_id=398848`, { waitUntil: "networkidle2" });
+      await page.waitForSelector(".red.bold");
+      await page.evaluate(() => document.querySelector('.search-text').value = "");
+      await page.type(".search-text", searchWord);
+      await page.click('#search-submit-button');
+      await page.waitForSelector(".red.bold");
+
+      for(let i = 1; i <= limit; i++) {
+        await page.click(`.result-item-list .result-item:nth-child(${i}) .result-preview img`);
+        await page.click('#preview-type-2');
+        htmlSnippets.push(await page.evaluate(async () => {
+          return document.querySelector('#active-preview-box .preview-source').value
+        }));
+        await page.waitForSelector("#fancybox-close", {visible: true})
+        await page.click("#fancybox-close");
+        await page.waitForSelector('#fancybox-overlay', {hidden: true});
+        console.log(`Scraped ${i}/${limit} for ${searchWord}`);
+      }
+      generateHTML(searchWord, htmlSnippets);
       browser.close();
       return resolve();
     } catch (e) {
@@ -59,19 +97,45 @@ const scrapeAnchorTags = (cookies) => {
   });
 }
 
-const main = async() => {
-  browser = await setUpBrowser();
-  page = await browser.newPage();
+const scrapeSnippets = async(searchWord) => {
+  console.log(`Start scraping for ${searchWord}`);
 
-  const cookies = await login();
+  const browser = await setUpBrowser();
+  const page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(0);
 
-  await scrapeAnchorTags(cookies);
+  const cookies = await login(page);
+  await scrapeBySearchWord(browser, page, searchWord, cookies);
+  console.log(`Ended scraping for ${searchWord}`);
 }
 
-main()
-  .then(() => {
-    console.log("Done scraping successfully");
-  })
-  .catch( e => {
-    console.log(e);
-  })
+const main = () => {
+
+  const keywords = ["ruby", "php", "javascript", "nodejs", "ruby on rails", "css", "linux", "docker", "react"];
+
+  fs.mkdir(path.join(__dirname, snippetsDir), err => {
+    if(err) throw err;
+    console.log(`Created a directory called "${snippetsDir}"`);
+  });
+
+  const promises = keywords.map(keyword => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        await scrapeSnippets(keyword);
+        resolve();
+      } catch (e) {
+        reject(e);
+      }
+    });
+  });
+
+  Promise.all(promises)
+    .then(() => {
+      console.log("Done successfully");
+    })
+    .catch(e => {
+      console.error(e);
+    });
+}
+
+main();
